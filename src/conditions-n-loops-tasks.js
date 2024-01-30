c569d51130a99badfc5a7ec3d87cc0657e000f4b
agamasha <mariaaga17@gmail.com>
2024-01-30 11:37:39 +0300

    more tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -525,8 +525,24 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let string = str;
+  for (let i = 0; i < iterations; i += 1) {
+    let evenPart = '';
+    let oddPart = '';
+    for (let j = 0; j < string.length; j += 1) {
+      if (j % 2 === 0) {
+        evenPart += string[j];
+      } else {
+        oddPart += string[j];
+      }
+    }
+    string = evenPart + oddPart;
+    if (string === str) {
+      string = shuffleChar(str, iterations % i);
+    }
+  }
+  return string;
 }
 
 /**
